b9c7f7a7d7840c9505d3e5a8b34418cd915e5776
Karen Zamlutti <zamlutti@gmail.com>
2012-01-19 13:52:14 -0200

    added search by term and search by category

--- src/client/PlaceSearcher.php
+++ src/client/PlaceSearcher.php
@@ -14,9 +14,8 @@ class PlaceSearcher
     }
 
     public function byRadius($radius, $latitude, $longitude, $term = null,
-        $categoryId = null, $startIndex = null)
+                             $categoryId = null, $startIndex = null)
     {
-
         $this->uriBuilder->withBase('/places/byradius');
         $this->uriBuilder->withParameter('radius', $radius);
         $this->uriBuilder->withParameter('latitude', $latitude);
@@ -28,6 +27,48 @@ class PlaceSearcher
 
         if (!is_null($categoryId)) {
             $this->uriBuilder->withParameter('category', $categoryId);
+        }
+
+        if (!empty($startIndex)) {
+            $this->uriBuilder->withParameter('start', $startIndex);
+        }
+
+        $response = $this->client->request($this->uriBuilder->build());
+        return $this->placesConverter->toPlaceResult($response);
+    }
+
+    public function byTerm($term, $state = null, $city = null, $startIndex = null)
+    {
+        $this->uriBuilder->withBase('/places/byterm');
+        $this->uriBuilder->withParameter('term', $term);
+
+        if (!empty($state)) {
+            $this->uriBuilder->withParameter('state', $state);
+        }
+
+        if (!empty($city)) {
+            $this->uriBuilder->withParameter('city', $city);
+        }
+
+        if (!empty($startIndex)) {
+            $this->uriBuilder->withParameter('start', $startIndex);
+        }
+
+        $response = $this->client->request($this->uriBuilder->build());
+        return $this->placesConverter->toPlaceResult($response);
+    }
+
+    public function byCategory($categoryId, $state = null, $city = null, $startIndex = null)
+    {
+        $this->uriBuilder->withBase('/places/bycategory');
+        $this->uriBuilder->withParameter('category', $categoryId);
+
+        if (!empty($state)) {
+            $this->uriBuilder->withParameter('state', $state);
+        }
+
+        if (!empty($city)) {
+            $this->uriBuilder->withParameter('city', $city);
         }
 
         if (!empty($startIndex)) {
--- tests/client/PlaceSearcherTest.php
+++ tests/client/PlaceSearcherTest.php
@@ -3,7 +3,6 @@ include_once('../Autoloader.php');
 
 class PlaceSearcherTest extends PHPUnit_Framework_TestCase
 {
-
     private $clientMocked;
     private $uriBuilderMocked;
     private $placesConverterMocked;
@@ -11,6 +10,10 @@ class PlaceSearcherTest extends PHPUnit_Framework_TestCase
     private $radius = 0.5;
     private $latitude = -23.45;
     private $longitude = -46.78;
+    private $term = "lalapopo";
+    private $category = 42;
+    private $state = "SP";
+    private $city = "São Paulo";
     private $baseUri = "base-uri";
     private $queryBuilt = 'query-built';
     private $placeResult;
@@ -19,23 +22,23 @@ class PlaceSearcherTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->clientMocked = $this->getMockBuilder('HttpClientWrapper')
-                ->disableOriginalConstructor()
-                ->getMock();
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $this->clientMocked->expects($this->any())
-                ->method('request')
-                ->will($this->returnValue($this->places));
+            ->method('request')
+            ->will($this->returnValue($this->places));
 
         $this->uriBuilderMocked = $this->getMock('UriBuilder');
         $this->uriBuilderMocked->expects($this->any())
-                ->method('build')
-                ->will($this->returnValue($this->queryBuilt));
+            ->method('build')
+            ->will($this->returnValue($this->queryBuilt));
 
         $this->placeResult = new PlaceResult();
         $this->placesConverterMocked = $this->getMock('PlacesConverter');
         $this->placesConverterMocked->expects($this->any())
-                ->method('toPlaceResult')
-                ->will($this->returnValue($this->placeResult));
+            ->method('toPlaceResult')
+            ->will($this->returnValue($this->placeResult));
 
 
         $this->placeSearcher = new PlaceSearcher($this->clientMocked,
@@ -45,8 +48,8 @@ class PlaceSearcherTest extends PHPUnit_Framework_TestCase
     public function testSearchByRadius()
     {
         $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
-                                                          $this->latitude,
-                                                          $this->longitude);
+            $this->latitude,
+            $this->longitude);
 
         $this->assertSame($this->placeResult, $placesRetrieved);
     }
@@ -54,11 +57,266 @@ class PlaceSearcherTest extends PHPUnit_Framework_TestCase
     public function testSearchByRadiusWithTerm()
     {
         $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
-                                                          $this->latitude,
-                                                          $this->longitude,
-                                                          'pizza');
+            $this->latitude,
+            $this->longitude,
+            'pizza');
 
         $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByRadiusWithTermAndCategory()
+    {
+        $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude,
+            $this->term,
+            $this->category);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByRadiusWithCategory()
+    {
+        $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude,
+            null,
+            0);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByRadiusWithStartIndex()
+    {
+        $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude,
+            null,
+            null,
+            10);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testThatSearchByRadiusCallPlaceRequest()
+    {
+        $this->clientMocked->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($this->queryBuilt));
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude);
+    }
+
+    public function testThatSearchByRadiusWithTermCallPlaceRequestWithTerm()
+    {
+        $this->clientMocked->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($this->queryBuilt));
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude,
+            'pizza');
+    }
+
+    public function testThatSearchByRadiusWithCategoryCallPlaceRequestWithCategory()
+    {
+        $this->clientMocked->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($this->queryBuilt));
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude,
+            null,
+            0);
+    }
+
+    public function testThatSearchByRadiusWithStartIndexCallPlaceRequestWithStartIndex()
+    {
+        $this->clientMocked->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($this->queryBuilt));
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude,
+            null,
+            null,
+            10);
+    }
+
+    public function testThatSearchByRadiusSetBaseQuery()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('withBase')
+            ->with($this->equalTo('/places/byradius'));
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude);
+    }
+
+    public function testThatSearchByRadiusAddsRequiredParameters()
+    {
+        $this->uriBuilderMocked->expects($this->exactly(3))
+            ->method('withParameter');
+
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude);
+    }
+
+    public function testThatSearchByRadiusCallsBuildQuery()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('build');
+
+        $this->placeSearcher->byRadius($this->radius,
+            $this->latitude,
+            $this->longitude);
+    }
+
+    public function testSearchByTerm()
+    {
+        $placesRetrieved = $this->placeSearcher->byTerm($this->term);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByTermFilteredByState()
+    {
+        $placesRetrieved = $this->placeSearcher->byTerm($this->term,
+            $this->state);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByTermFilteredByStateAndCity()
+    {
+        $placesRetrieved = $this->placeSearcher->byTerm($this->term,
+            $this->state,
+            $this->city);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByTermWithStartIndex()
+    {
+        $placesRetrieved = $this->placeSearcher->byTerm($this->term,
+            null,
+            null,
+            10);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testThatSearchByTermCallPlaceRequest()
+    {
+        $this->clientMocked->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($this->queryBuilt));
+
+        $this->placeSearcher->byTerm($this->term);
+    }
+
+    public function testThatSearchByTermSetBaseQuery()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('withBase')
+            ->with($this->equalTo('/places/byterm'));
+
+        $this->placeSearcher->byTerm($this->term);
+    }
+
+    public function testThatSearchByTermAddsRequiredParameters()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('withParameter');
+
+
+        $this->placeSearcher->byTerm($this->term);
+    }
+
+    public function testThatSearchByTermCallsBuildQuery()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('build');
+
+        $this->placeSearcher->byTerm($this->term);
+    }
+
+    public function testSearchByCategory()
+    {
+        $placesRetrieved = $this->placeSearcher->byCategory($this->category,
+            $this->state,
+            $this->city);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByCategoryFilteredByState()
+    {
+        $placesRetrieved = $this->placeSearcher->byCategory($this->category,
+            $this->state);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByCategoryFilteredByStateAndCity()
+    {
+        $placesRetrieved = $this->placeSearcher->byCategory($this->category,
+            $this->state,
+            $this->city);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testSearchByCategoryWithStartIndex()
+    {
+        $placesRetrieved = $this->placeSearcher->byCategory($this->category,
+            null,
+            null,
+            10);
+
+        $this->assertSame($this->placeResult, $placesRetrieved);
+    }
+
+    public function testThatSearchByCategoryCallPlaceRequest()
+    {
+        $this->clientMocked->expects($this->once())
+            ->method('request')
+            ->with($this->equalTo($this->queryBuilt));
+
+        $this->placeSearcher->byCategory($this->category);
+    }
+
+    public function testThatSearchByCategorySetBaseQuery()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('withBase')
+            ->with($this->equalTo('/places/bycategory'));
+
+        $this->placeSearcher->byCategory($this->category);
+    }
+
+    public function testThatSearchByCategoryAddsRequiredParameters()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('withParameter');
+
+        $this->placeSearcher->byCategory($this->category);
+    }
+
+    public function testThatSearchByCategoryCallsBuildQuery()
+    {
+        $this->uriBuilderMocked->expects($this->once())
+            ->method('build');
+
+        $this->placeSearcher->byCategory($this->category);
     }
 
     public function testThatSearchByUri()
@@ -68,116 +326,11 @@ class PlaceSearcherTest extends PHPUnit_Framework_TestCase
         $this->assertSame($this->placeResult, $placesRetrieved);
     }
 
-    public function testSearchByRadiusWithCategory()
-    {
-        $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
-                                                          $this->latitude,
-                                                          $this->longitude,
-                                                          null,
-                                                          0);
-
-        $this->assertSame($this->placeResult, $placesRetrieved);
-    }
-
-    public function testSearchByRadiusWithStartIndex()
-    {
-        $placesRetrieved = $this->placeSearcher->byRadius($this->radius,
-                                                          $this->latitude,
-                                                          $this->longitude,
-                                                          null,
-                                                          null,
-                                                          10);
-
-        $this->assertSame($this->placeResult, $placesRetrieved);
-    }
-
-    public function testThatSearchByRadiusCallPlaceRequest()
-    {
-        $this->clientMocked->expects($this->once())
-                ->method('request')
-                ->with($this->equalTo($this->queryBuilt));
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude);
-    }
-
-    public function testThatSearchByRadiusWithTermCallPlaceRequestWithTerm()
-    {
-        $this->clientMocked->expects($this->once())
-                ->method('request')
-                ->with($this->equalTo($this->queryBuilt));
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude,
-                                       'pizza');
-    }
-
-    public function testThatSearchByRadiusWithCategoryCallPlaceRequestWithCategory()
-    {
-        $this->clientMocked->expects($this->once())
-                ->method('request')
-                ->with($this->equalTo($this->queryBuilt));
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude,
-                                       null,
-                                       0);
-    }
-
-    public function testThatSearchByRadiusWithStartIndexCallPlaceRequestWithStartIndex()
-    {
-        $this->clientMocked->expects($this->once())
-                ->method('request')
-                ->with($this->equalTo($this->queryBuilt));
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude,
-                                       null,
-                                       null,
-                                       10);
-    }
-
-    public function testThatSearchByRadiusSetBaseQuery()
-    {
-        $this->uriBuilderMocked->expects($this->once())
-                ->method('withBase')
-                ->with($this->equalTo('/places/byradius'));
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude);
-    }
-
-    public function testThatSearchByRadiusAddsRequiredParameters()
-    {
-        $this->uriBuilderMocked->expects($this->exactly(3))
-                ->method('withParameter');
-
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude);
-    }
-
-    public function testThatSearchByRadiusCallsBuildQuery()
-    {
-        $this->uriBuilderMocked->expects($this->once())
-                ->method('build');
-
-        $this->placeSearcher->byRadius($this->radius,
-                                       $this->latitude,
-                                       $this->longitude);
-    }
-
     public function testThatSearchByUriCallPlaceRequest()
     {
         $this->clientMocked->expects($this->once())
-                ->method('request')
-                ->with($this->equalTo($this->baseUri));
+            ->method('request')
+            ->with($this->equalTo($this->baseUri));
 
         $this->placeSearcher->byUri($this->baseUri);
     }
